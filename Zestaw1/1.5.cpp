19f03399df2e93181ff9af954f51fe2eed5332ea
boojtus <wojciech.ciolczyk@smcebi.edu.pl>
2019-03-05 14:59:26 +0100

    Update 1.5.cpp

--- Zestaw1/1.5.cpp
+++ Zestaw1/1.5.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include <iomanip>
+#include <cmath>
 
 using namespace std;
-
+int n;
+double zaokraglij(double x)
+{
+  int y = x * pow(10,n+1);
+  if (y % 10 >= 5) y += 10;
+  return (y / 10) * 1/pow(10,n);
+}
 int main()
 {
-    double x,y,n;
+    double x,y;
     cout << "Podaj a" << endl;
     cin >> x;
     cout << "Podaj b" << endl;
@@ -13,9 +19,9 @@ int main()
     cout<<"Do ilu miejsc po przecinku"<<endl;
     cin>>n;
 
-    cout<<setprecision(n+1)<< "a+b= " <<x+y<< endl;
-    cout <<setprecision(n+1)<< "a-b= " <<x-y<< endl;
-    cout <<setprecision(n+1)<< "a*b= " <<x*y<< endl;
-    cout <<setprecision(n+1)<< "a/b= " <<x/y<< endl;
+    cout << "a+b= " <<zaokraglij(x+y)<< endl;
+    cout << "a-b= " <<zaokraglij(x-y)<< endl;
+    cout << "a*b= " <<zaokraglij(x*y)<< endl;
+    cout << "a/b= " <<zaokraglij(x/y)<< endl;
     return 0;
 }
