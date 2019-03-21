6b4254fa8925186c3336e9a28fc8cb1424105967
boojtus <44576500+boojtus@users.noreply.github.com>
2019-03-21 10:34:02 +0100

    Zestaw3
    Zestaw3

---
+++ 3.2.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool anagram(string wyraz1, string wyraz2)
+{
+    if (wyraz1.length()!=wyraz2.length())
+        return false;
+
+    for (int i = 0; i<wyraz1.length()-1; i++)
+    {
+        for (int j = 0; j<wyraz2.length()-1; j++)
+        {
+            if (wyraz1[j]>wyraz1[j+1])
+                swap(wyraz1[j], wyraz1[j+1]);
+
+            if (wyraz2[j]>wyraz2[j+1])
+                swap(wyraz2[j], wyraz2[j+1]);
+        }
+    }
+
+    return wyraz1==wyraz2;
+}
+
+int main()
+{
+    string wyraz1, wyraz2;
+
+    cout << "Podaj wyraz pierwszy" << endl;
+    cin >> wyraz1;
+
+    cout << "Podaj wyraz drugi" << endl;
+    cin >> wyraz2;
+
+    if(anagram(wyraz1, wyraz2)) cout<<"Anagram"<<endl;
+    else cout<<"Nie anagram"<<endl;
+    return 0;
+}
---
+++ 3.3.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include<string>
+#include <ctime>
+
+
+using namespace std;
+
+int sprawdz(string txt,string x)
+{
+    if(x.length() > txt.length()){return -1;}
+    else if(x.length() < txt.length())
+    {
+        for(int i=0;i<txt.length();i++)
+        {
+            fo
+        }
+    }
+}
+
+
+
+int main()
+{
+    string txt;
+    string x;
+    int lwystapien=0;
+    cout<<"Podaj pierwszy ciag znakow"<<endl;
+    getline(cin,txt);
+    cout<<"Podaj drugi ciag znakow"<<endl;
+    getline(cin,x);
+
+ return 0;
+}
